d02d03c1a2fb7c249dd91c7a2ec1c2b640cae68c
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-03 11:08:08 +0400

    Using the Test Method

---
+++ ex1.js
@@ -0,0 +1,5 @@
+// 1. Примените регулярное выражение myRegex к строке, myString используя .test()метод.
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
