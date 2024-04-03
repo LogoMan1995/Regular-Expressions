3bdf34aeb38a739e8720643c157d00a1496fc168
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-03 11:27:34 +0400

    Extract Matches

---
+++ ex5.js
@@ -0,0 +1,8 @@
+// 5. Примените .match() метод для извлечения строки coding.
+
+
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result = extractStr.match(codingRegex);
+
