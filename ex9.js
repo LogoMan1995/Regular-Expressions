1dcacf6ea5d52b0df137fd7e4c0b8a2bddab9bf8
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-03 12:19:45 +0400

    Match Letters of the Alphabet

---
+++ ex9.js
@@ -0,0 +1,9 @@
+// 9. Сопоставьте все буквы в строке quoteSample.
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex); 
+
+
+
+
