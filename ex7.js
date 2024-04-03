664e38df38eb32791a26038dbc30d42d0224fb72
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-03 11:47:13 +0400

    Match Anything with Wildcard Period

---
+++ ex7.js
@@ -0,0 +1,9 @@
+// 7.Заполните регулярное выражение unRegex так, чтобы оно соответствовало строкам run, sun, fun, pun, nun и bun. Ваше регулярное выражение должно использовать подстановочный знак.
+
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; 
+let result = unRegex.test(exampleStr);
+
+
+
